c4349d56fa5751129f30e8ae34f566e048803275
Matheus Melo <matheuslmelo.dev@gmail.com>
2026-03-10 19:02:04 -0300

    feat: add JPA persistence and core domain entities

--- src/main/java/com/matheus/workout_api/entity/User.java
+++ src/main/java/com/matheus/workout_api/entity/User.java
@@ -1,4 +1,98 @@
 package com.matheus.workout_api.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @JsonIgnore
+    @Column(name = "password_hash")
+    private String password;
+
+    @Column(name = "height_cm")
+    private double height;
+
+    @Column(name = "weight_kg")
+    private double weight;
+
+    @Column(name = "goal")
+    private String goal;
+
+    public User() {
+    }
+
+    public User(String name, String email, String password, double height, double weight, String goal) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.height = height;
+        this.weight = weight;
+        this.goal = goal;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getGoal() {
+        return goal;
+    }
+
+    public void setGoal(String goal) {
+        this.goal = goal;
+    }
 }
--- src/main/java/com/matheus/workout_api/enums/DifficultyLevel.java
+++ src/main/java/com/matheus/workout_api/enums/DifficultyLevel.java
@@ -1,6 +1,7 @@
 package com.matheus.workout_api.enums;
 
 public enum DifficultyLevel {
+
     EASY,
     NORMAL,
     HARD
